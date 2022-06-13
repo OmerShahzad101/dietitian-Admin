63ea8cf07986b4b2bfde4c6e445551215690a10e
Omer Shahzad <omershahzad707@gmail.com>
2022-06-13 17:56:03 +0500

    blog

--- admin-backend/src/api/controllers/admin/blog.controller.js
+++ admin-backend/src/api/controllers/admin/blog.controller.js
@@ -2,18 +2,20 @@ const mongoose = require("mongoose");
 const { checkDuplicate } = require("../../../config/errors");
 const Blog = require("../../models/blog.model");
 
-// API to create blog
+
 exports.create = async (req, res, next) => {
   try {
     let payload = req.body;
-    console.log("req", payload);
-    const blog = new Blog(payload);
-    await blog.save();
-
+    if (req.files && req.files.image) {
+      const images = req.files.image[0];
+      payload.image = `/${images.filename}`;
+    }
+    const newBlog = new Blog(payload);
+    const savedBlog = await newBlog.save();
     return res.send({
       success: true,
       message: "Blog created successfully",
-      blog,
+      savedBlog,
     });
   } catch (error) {
     if (error.code === 11000 || error.code === 11001)
@@ -22,86 +24,6 @@ exports.create = async (req, res, next) => {
   }
 };
 
-// API to edit blog
-exports.edit = async (req, res, next) => {
-  try {
-    let payload = req.body;
-    if (payload._id) {
-      const blog = await Blog.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(payload._id) },{ $set: payload },{new: true });
-
-      if (blog)
-        return res.json({
-          success: true,
-          message: "Blog Update successfully",
-          blog,
-        });
-      else
-        return res
-          .status(400)
-          .send({ success: false, message: "Blog not found for given Id" });
-    } else
-      return res
-        .status(400)
-        .send({ success: false, message: "Blog Id is required" });
-  } catch (error) {
-    if (error.code === 11000 || error.code === 11001)
-      checkDuplicate(error, res, "Blog");
-    else return next(error);
-  }
-}
-
-
-// API to delete Blog
-exports.delete = async (req, res, next) => {
-  try {
-    const { blogId } = req.params;
-    if (blogId) {
-      const blog = await Blog.deleteOne({ _id: blogId });
-      if (blog.deletedCount)
-        return res.send({
-          success: true,
-          message: "Blog deleted successfully",
-        });
-      else
-        return res
-          .status(400)
-          .send({ success: false, message: "Blog not found for given Id" });
-    } else
-      return res
-        .status(400)
-        .send({ success: false, message: "Blog Id is required" });
-  } catch (error) {
-    return next(error);
-  }
-};
-
-// API to get an blog
-exports.get = async (req, res, next) => {
-  try {
-    const { blogId } = req.params;
-    if (blogId) {
-      const blog = await Blog.findOne({ _id: mongoose.Types.ObjectId(blogId) },{ __v: 0, createdAt: 0, updatedAt: 0 }).lean(true);
-
-      if (blog)
-        return res.json({
-          success: true,
-          message: "Blog retrieved successfully",
-          blog,
-        });
-      else
-        return res
-          .status(400)
-          .send({ success: false, message: "Blog not found for given Id" });
-    } else
-      return res
-        .status(400)
-        .send({ success: false, message: "Blog Id is required" });
-  } catch (error) {
-    return next(error);
-  }
-};
-
-// API to get blog list
 exports.list = async (req, res, next) => {
   try {
     let { page, limit } = req.query;
@@ -115,51 +37,22 @@ exports.list = async (req, res, next) => {
     page = page !== undefined && page !== "" ? parseInt(page) : 1;
     limit = limit !== undefined && limit !== "" ? parseInt(limit) : 10;
 
-    // const total = await Blog.countDocuments({})
+    const total = await Blog.countDocuments(filters);
 
-    const totalBlogs = await Blog.aggregate([
-      {
-        $project: {
-          _id: 1,
-          title: 1,
-          image: 1,
-          description: 1,
-          email: 1,
-          category: 1,
-          createdAt: 1,
-          status: 1,
-        },
-      },
-      { $match: filters },
-      { $count: "total" },
-    ]);
+    // if (page > Math.ceil(total / limit) && total > 0)
+    //   page = Math.ceil(total / limit);
 
-    const blogList = await Blog.aggregate([
+    const blog = await Blog.aggregate([
       { $match: filters },
       { $sort: { createdAt: -1 } },
       { $skip: limit * (page - 1) },
-      {
-        $project: {
-          _id: 1,
-          title: 1,
-          image: 1,
-          description: 1,
-          email: 1,
-          category: 1,
-          createdAt: 1,
-          status: 1,
-        },
-      },
       { $limit: limit },
     ]);
-
-    let total = totalBlogs[0] ? totalBlogs[0].total : "";
-
     return res.send({
       success: true,
-      message: `blog's fetched successfully`,
+      message: "services are fetched successfully",
       data: {
-        blogList,
+        blog,
         pagination: {
           page,
           limit,
@@ -168,6 +61,85 @@ exports.list = async (req, res, next) => {
         },
       },
     });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.get = async (req, res, next) => {
+  try {
+    const { blogId } = req.params;
+    if (blogId) {
+      const blog = await Blog.findOne(
+        { _id: mongoose.Types.ObjectId(blogId) },
+        { __v: 0, createdAt: 0, updatedAt: 0 }
+      ).lean(true);
+
+      if (blog)
+        return res.json({
+          success: true,
+          message: "blog retrieved successfully",
+          blog,
+        });
+      else
+        return res
+          .status(400)
+          .send({ success: false, message: "blog not found for given Id" });
+    } else
+      return res
+        .status(400)
+        .send({ success: false, message: "blog Id is required" });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.edit = async (req, res, next) => {
+  // console.log('-------------',req.body);
+  try {
+    let payload = req.body;
+    if (req.files && req.files.logo) {
+      const image = req.files.logo[0];
+      // const imgData = fs.readFileSync(image.path)
+      // payload.logo = await addImage(imgData)
+      payload.logo = `/${image.filename}`;
+    }
+    const service = await Services.findByIdAndUpdate(
+      { _id: mongoose.Types.ObjectId(payload._id) },
+      { $set: payload },
+      { new: true }
+    );
+    return res.send({
+      success: true,
+      message: "services updated successfully",
+      service,
+    });
+  } catch (error) {
+    if (error.code === 11000 || error.code === 11001)
+      checkDuplicate(error, res, "Member");
+    else return next(error);
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  try {
+    const { serviceId } = req.params;
+    if (serviceId) {
+      const service = await Services.deleteOne({ _id: serviceId });
+      if (service && service.deletedCount)
+        return res.send({
+          success: true,
+          message: "service is  deleted successfully",
+          serviceId,
+        });
+      else
+        return res
+          .status(400)
+          .send({ success: false, message: "service not found for given Id" });
+    } else
+      return res
+        .status(400)
+        .send({ success: false, message: "service Id is required" });
   } catch (error) {
     return next(error);
   }
